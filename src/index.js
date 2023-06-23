9803437408eed8d099fe028f5a3c98490e17a670
peyre.dev <peyre.dev@yahoo.com>
2023-06-23 16:37:27 +0200

    database config connexion

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+const connection = require('./config/database');
 const express = require('express')
 const app = express()
 const port = 3000
+
+/*
+    Look for let connexion.connect here or in the config/database.js
+*/
+//connection.connect();
 
 app.listen(port, () => {
     console.log(`Library_stock_api listening on port ${port}`)
